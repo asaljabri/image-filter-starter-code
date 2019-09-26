84c67c2471ebb747d42fdc382bcd34bf3d5b4591
Ahmad Alharbi <ahmadalharbi@Ahmads-MacBook-Air.local>
2019-09-26 14:13:04 +0300

    seconde project Ahmed alharbi

---
+++ src/util/util.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+//import  Jimp = require('jimp');
+import Jimp from 'jimp';
+
+
+
+export async function filterImageFromURL(inputURL: string): Promise<string>{
+    return new Promise( async resolve => {
+        const photo = await Jimp.read(inputURL);
+        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+        await photo
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(__dirname+outpath, (img)=>{
+            resolve(__dirname+outpath);
+        });
+    });
+}
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files:Array<string>){
+    for( let file of files) {
+        fs.unlinkSync(file);
+    }
+}
+
---
+++ www/server.js
@@ -0,0 +1,66 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const util_1 = require("./util/util");
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    // Init the Express application
+    const app = express_1.default();
+    // Set the network port
+    const port = process.env.PORT || 8082;
+    // Use the body parser middleware for post requests
+    app.use(body_parser_1.default.json());
+    // GET /filteredimage?image_url={{URL}}
+    // endpoint to filter an image from a public url.
+    // IT SHOULD
+    //    1
+    //    1. validate the image_url query
+    //    2. call filterImageFromURL(image_url) to filter the image
+    //    3. send the resulting file in the response
+    //    4. deletes any files on the server on finish of the response
+    // QUERY PARAMATERS
+    //    image_url: URL of a publicly accessible image
+    // RETURNS
+    //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+    /**************************************************************************** */
+    app.get('/filteredimage/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        let { image_url } = req.query;
+        if (!image_url) {
+            return res.status(400).send({ message: 'Image URL is required' });
+        }
+        // Write to console the URL received
+        console.log(`Image URL: ${image_url}`);
+        let filteredpath = yield util_1.filterImageFromURL(image_url);
+        //Write to console the local path to the image
+        console.log(`Path to downloaded image: ${filteredpath}`);
+        res.status(200).sendFile(filteredpath);
+        //Runs after file is downloaded to delete the local file
+        res.on('finish', function () {
+            util_1.deleteLocalFiles([filteredpath]);
+            console.log("Deleted local file");
+        });
+    }));
+    // Root Endpoint
+    // Displays a simple message to the user
+    app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        res.send("try GET /filteredimage?image_url={{}}");
+    }));
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+}))();
+//# sourceMappingURL=server.js.map
